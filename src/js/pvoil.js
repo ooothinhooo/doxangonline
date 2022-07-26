e9e6cae8e961358a959e6223721953f6b2f74c42
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2022-07-26 23:05:19 +0700

    update giá xăng

--- src/js/pvoil.js
+++ src/js/pvoil.js
@@ -1,9 +1,10 @@
 pvoil = [{
         Name: 'Xăng RON 95-III',
-        price: 32.760,
+       // price: 32.760,
+       price:26.070,
     },
     {
         Name: 'Xăng RON 92-II',
-        price: 31.300,
+        price: 25.070,
     }
 ]
